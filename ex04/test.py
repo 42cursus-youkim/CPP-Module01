8349546d9fa9253c7815c9d05215d3f9ab66a330
scarf005 <greenscarf005@gmail.com>
2022-03-24 17:08:11 +0900

    test: :white_check_mark: update test

--- ex04/test.py
+++ ex04/test.py
@@ -1,4 +1,4 @@
-#!/bin/env python3
+#!/usr/bin/env python3
 
 from pathlib import Path
 from subprocess import run
@@ -7,17 +7,24 @@ from hypothesis import example, given
 from hypothesis import strategies as st
 from termcolor import cprint
 
+EXIT_FAILURE = 1
 
-@given(st.text(), st.text())
+
+@given(st.text(min_size=1), st.text(min_size=1))
 @example("para", "PARA")
+@example("para", "")
+@example(" ", "!!!!")
 def test_replace_works(word: str, to: str):
-    cprint(f"case: {word=} {to=}", "yellow")
-    if to == r"\x00":
+    cprint(f"case: <{word}> => <{to=}>", "yellow")
+    param = ["./prog.out", "test.txt", word, to]
+    try:
+        result = run(param)
+    except ValueError:
+        cprint(f"skipping due to {word} or {to} a NULL byte", "magenta")
         return
-    param = ["./replace", "test.txt", word, to]
-    result = run(param)
-    if len(word) == 0 or len(param) != 4:
-        assert result.returncode == 1
+
+    if len(param) != 4:
+        assert result.returncode == EXIT_FAILURE
 
     else:
         text = Path("test.replace").read_text()
@@ -25,5 +32,19 @@ def test_replace_works(word: str, to: str):
         assert text == answer
 
 
+def test_invalid_file():
+    param = ["./prog.out", "DOES.NOT.EXIST", "para", "PARA"]
+    assert run(param).returncode == EXIT_FAILURE
+
+
+def test_invalid_arg():
+    param = ["./prog.out"]
+    for _ in range(6):
+        assert run(param).returncode == EXIT_FAILURE
+        param.append("a")
+
+
 if __name__ == "__main__":
     test_replace_works()
+    test_invalid_file()
+    test_invalid_arg()
