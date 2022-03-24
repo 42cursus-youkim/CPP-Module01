c6da6003cc4f8decfea28db0d57fe84324d019e3
scarf005 <greenscarf005@gmail.com>
2022-03-24 16:31:32 +0900

    feat: :sparkles: changed subject pdf

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
 #include <string>
 
+using std::cout;
+
 int main(void) {
   std::string string = "HI THIS IS BRAIN";
   std::string* stringPTR = &string;
   std::string& stringREF = string;
 
-  std::cout << "address using <string>          :" << &string << "\n";
-  std::cout << "address using <string pointer>  :" << stringPTR << "\n";
-  std::cout << "address using <string referebce>:" << &stringREF << "\n";
+  cout << "[[[Printing address of string]]]\n";
+  cout << "address of <string>          :" << &string << "\n";
+  cout << "address of <string pointer>  :" << stringPTR << "\n";
+  cout << "address of <string reference>:" << &stringREF << "\n";
+
+  cout << "\n[[[Printing value of string]]]\n";
+  cout << "value of <string>            :" << string << "\n";
+  cout << "value of <string pointer>    :" << *stringPTR << "\n";
+  cout << "value of <string reference>  :" << stringREF << "\n";
 }
