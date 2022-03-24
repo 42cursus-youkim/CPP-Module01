35935a0bff0cf344e65140d64e77cd8841f81185
scarf005 <greenscarf005@gmail.com>
2022-03-24 17:03:54 +0900

    fix: :bug: handle exception

--- ex04/src/main.cpp
+++ ex04/src/main.cpp
@@ -1,5 +1,7 @@
+// #include <stdlib.h>
 #include <fstream>
 #include <iostream>
+#include <stdexcept>
 #include <string>
 #include "Replace.hpp"
 
@@ -17,7 +19,13 @@ int main(int argc, const char* argv[]) {
     std::cout << "Target string cannot be empty" << std::endl;
     return 1;
   }
-  Replace replace(fileName, target, replaceWith);
-  replace.createReplaceFile();
+  try {
+    Replace replace(fileName, target, replaceWith);
+    replace.createReplaceFile();
+  } catch (std::exception& e) {
+    std::cout << e.what() << std::endl;
+    return 1;
+  }
+  // system("leaks prog.out | grep 'leaks for'");
   return (0);
 }
